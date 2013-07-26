47ef5dadf6156d5763ff17df0b82099643170d24
dariusz gorzeba <dgorzeba@gmail.com>
2013-07-26 11:14:46 +0200

    init

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'active_model_serializers'
+gem 'state_machine'
+
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+# gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Use unicorn as the app server
+# gem 'unicorn'
+#
+group :development, :test do
+  gem 'quiet_assets'
+  gem 'dotenv-rails'
+  gem 'pry-debugger'
+  gem 'pry'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gapi::Application.config.secret_key_base = '93a08ffce75eefdf99dfac11dad108d48ece542512bec89239b767ece81a3229a82d22c05228ad915a67a666a016f219817eabd56b85a2102930428d65c0017a'
