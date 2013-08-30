171f4f5910a512d647f79025882efcf87dce0525
kv109 <kacper@walanus.com>
2013-08-30 14:18:07 +0200

    fix

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -2,7 +2,7 @@ class OrderSerializer < ActiveModel::Serializer
   self.root = false
 
 
-  attributes :_id, :name, :ordered_at, :founder, :dishes
+  attributes :_id, :name, :ordered_at, :founder, :founder_uid, :dishes
 
   def founder
     founder = User.where(uid: object.founder_uid).first
