f0ed871f8a2e77b0245e0249125d7df8855e7b41
Gracjan Grala <gracjan.grala@elpassion.pl>
2013-11-12 19:42:08 +0100

    show user gravatar in navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
+  require 'digest/md5'
   attr_accessible :name, :provider, :uid, :refresh_token, :access_token, :expires, :email
 
   validates_uniqueness_of :uid, :scope => :provider
+
+  def gravatar_url
+    'http://www.gravatar.com/avatar/' + Digest::MD5.hexdigest(email)
+  end
 end
