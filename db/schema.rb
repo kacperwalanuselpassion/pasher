9c66cafb5a7e498df5c6ade56f8426d653feadbf
kv109 <kacper@walanus.com>
2014-01-02 16:31:53 +0100

    Dish#user_uid to #users_uid migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112174049) do
+ActiveRecord::Schema.define(version: 20140102141449) do
 
   create_table "users", force: true do |t|
     t.string   "provider"
