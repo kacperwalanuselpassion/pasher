6aff86dc9bdda7decdf69f83947b306994d7e6eb
kv109 <kacper@walanus.com>
2014-01-03 10:19:57 +0100

    data structure and layout for joinable dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,7 +3,7 @@ class Dish
   extend ActiveModel::Naming
   include ActiveModel::SerializerSupport
 
-  attr_accessor :_id, :name, :description, :price, :users_uids, :order_uid, :storage
+  attr_accessor :_id, :name, :description, :price, :users_uids, :order_uid, :storage, :joinable, :participants_limit
 
   def initialize
     self.storage = Storage::Mongo::Dish
--- app/serializers/dish_serializer.rb
+++ app/serializers/dish_serializer.rb
@@ -1,7 +1,7 @@
 class DishSerializer < ActiveModel::Serializer
   self.root = false
 
-  attributes :_id, :name, :description, :price, :users_names, :users_uids, :order_uid
+  attributes :_id, :name, :description, :price, :users_names, :users_uids, :order_uid, :joinable, :participants_limit
 
   def users_names
     object.users.map(&:name).join(', ')
